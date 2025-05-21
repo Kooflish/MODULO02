6ea5eddf761aa3f54667dcce6db606b7cff344b5
Kooflish <kgonzalo9812@gmail.com>
2025-05-21 14:25:12 -0400

    CORRECCION DE EJERCECIO

---
+++ Ordenarnumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+        Console.WriteLine("Digite 10 números (uno por uno):");
+
+        // Leer los 10 números y guardarlos en el arreglo
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write("Número " + (i + 1) + ": ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Ordenar el arreglo
+        Array.Sort(numeros);
+
+        // Mostrar los números ordenados
+        Console.WriteLine("\nNúmeros ordenados:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
